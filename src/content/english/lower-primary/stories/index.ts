64e90eaf90c8dea89758392223bd53cda8a52c31
swach-spec <kingoriwachira88@gmail.com>
2026-09-10 23:25:41 +0300

    Add expanded Story Forest catalogue

--- src/content/english/lower-primary/stories/index.ts
+++ src/content/english/lower-primary/stories/index.ts
@@ -3,10 +3,12 @@ import { lostKite } from "./lost-kite";
 import { cleverTortoise } from "./clever-tortoise";
 import { lionAndMouse } from "./lion-and-mouse";
 import { secretGarden } from "./secret-garden";
+import { expandedLowerPrimaryStories } from "./expanded-stories";
 
 export const lowerPrimaryEnglishStories: Story[] = [
   lostKite,
   cleverTortoise,
   lionAndMouse,
   secretGarden,
+  ...expandedLowerPrimaryStories,
 ];
